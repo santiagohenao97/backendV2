69764062d77b69d89da1b26772a494977a0801f1
Santiago Henao <51752344+santiagohenao97@users.noreply.github.com>
2024-08-15 23:55:51 -0500

    Add files via upload

---
+++ src/utils/services/WebServices.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+
+const CURRENT_API_URL = process.env.API_URL;
+const COMMON_HEADERS = { 'Content-Type': 'application/json' }
+
+const POSTData = async (endpoint, params = {}, customHeaders) => {
+  try {
+    const response = await axios.post(`${CURRENT_API_URL}${endpoint}`, params, {
+      headers: customHeaders ? { ...customHeaders, ...COMMON_HEADERS } : COMMON_HEADERS
+    })
+    if(!response) throw new Error('Bad response!')
+    return response
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+const PUTData = async (endpoint, params = {}, customHeaders) => {
+  try {
+    const response = await axios.put(`${CURRENT_API_URL}${endpoint}`, params, {
+      headers: customHeaders ? { ...customHeaders, ...COMMON_HEADERS } : COMMON_HEADERS
+    })
+    if(!response) throw new Error('Bad response!')
+    return response
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+const DELETEData = async (endpoint, customHeaders) => {
+  try {
+      const response = await axios.delete(`${CURRENT_API_URL}${endpoint}`, {
+          headers: customHeaders ? { ...customHeaders, ...COMMON_HEADERS } : COMMON_HEADERS
+      });
+      if (!response) throw new Error('Bad response!');
+      return response;
+  } catch (err) {
+      console.error(err);
+      throw err;
+  }
+};
+
+
+module.exports = {
+  POSTData,
+  PUTData,
+  DELETEData
+}
